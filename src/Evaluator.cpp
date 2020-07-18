31757ee11ce794720b98d9827e1914b2215fc06e
Sangami Ilangovan <sangamiilango94@gmail.com>
2020-07-18 15:26:30 +0200

    Reading input from file

--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main(int argc, char* argv[])
-{
+int main (int argc, char* argv[]) {
 
-return 0;
+  std::ifstream ipfile (argv[1]);
+  std::string line;
+  std::vector<std::string> input;
+
+  //Read lines from the input file into a vector that stores only the expressions
+  if (ipfile.is_open())
+  {
+    while(std::getline(ipfile,line))
+    {
+        //Ignore comments
+        if(line[0]!='#')
+        {
+            input.push_back(line);
+        }    
+    }
+    ipfile.close();
+  }
+  else 
+  std::cout << "Unable to open file"; 
+
+    // for(auto str:input)
+    // {
+    //     std::cout<<str<<std::endl;
+    // }
+
+  return 0;
 }
