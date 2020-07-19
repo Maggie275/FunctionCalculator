fbf8673dc29f236f22b6d27d52e223e5c1a3ad25
Sangami Ilangovan <sangamiilango94@gmail.com>
2020-07-19 19:37:50 +0200

    Error Handling and Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # FunctionCalculator
 Function Calculator used to evaluate mathematical expressions.
+
+The following arithmetic operations are supported
+* Addition
+* Subtraction
+* Multiplication
+* Addition
+
+Note:
+* Some example formats :
+```
+    (9.25*((12.5+1.3)-5.8))/(0.125*((13.2+2.8)*2))
+    
+    ((2.80-4.3)*((((9.3-2.3))+7)/3.5))
+```
+* a(b+c) is considered as a*(b+c) Eg. 2.1(4 + 3.6) --> 2.1*(4 + 3.6)
+* Any empty space is ignored
+* Other deviations to the format is considered as syntax error
+* All comments in the input text must begin with #. 
+    Otherwise they are treated as expressions with invalid characters.
+* Sample input file provided for reference
+
+To build and run :
+
+```
+cmake -Bbuild -H.
+cd build && make 
+```
+
+Create or update the input file and pass it with the executable argument:
+```
+./evaluator <input file path>
+```
+
--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -3,139 +3,235 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <algorithm>
+
+enum TokenType
+{
+    eTYPE_DIGIT = 0,
+    eTYPE_OPEN_BRACE,
+    eTYPE_CLOSE_BRACE,
+    eTYPE_OPERATOR,
+    eTYPE_NONE = 255
+};
+
+int precedence(char op)
+{
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
+}
+
+bool validateOperator(char op)
+{
+    return (op == '+' || op == '-' || op == '*' || op == '/') ? true : false;
+}
 
 float operate(float num1, float num2, char op)
 {
-    switch(op)
+    switch (op)
     {
-        case '+':
-                return num1 + num2;
-                break;
-        case '-':
-                return num1 - num2;
-                break;
-        case '*':
-                return num1 * num2;
-                break;
-        case '/':
-                return num1 / num2;
-                break;
-        default:
-                std::cout<<"Invalid operator"<<std::endl;
+    case '+':
+        return num1 + num2;
+    case '-':
+        return num1 - num2;
+    case '*':
+        return num1 * num2;
+    case '/':
+        return num1 / num2;
+    default:
+        std::cout << "Invalid operator" << std::endl;
+        return 0;
     }
 }
 
-void evaluate(std::string exp)
+void apply(std::stack<float> &digits, std::stack<char> &symbols)
 {
-    //std::cout<<exp<<std::endl;
-    std::stack<float> numbers;
-    std::stack<char> symbols;
-    for (auto i = 0; i < exp.size(); i++)
+    char op = symbols.top();
+    if (!symbols.empty())
     {
-        //Identify numbers
-        if (isdigit(exp[i])){
-            std::string test;
-            /*After first occurance of a digit, iterate through the string 
-            to get the complete floating point number by checking the subsequent characters.
-            The end of a floating point number is identified by the next occurance of a symbol.*/
-            
-            while (i < exp.size())
+        symbols.pop();
+    }
+    float num1 = digits.top();
+    if (!digits.empty())
+    {
+        digits.pop();
+    }
+    float num2 = digits.top();
+    if (!digits.empty())
+    {
+        digits.pop();
+    }
+    digits.push(operate(num2, num1, op));
+}
+
+void evaluate(const std::string &exp)
+{
+    //std::cout << exp << std::endl;
+    std::stack<float> digits;
+    std::stack<char> symbols;
+    TokenType prevTok = eTYPE_NONE;
+
+    if (std::count(exp.begin(), exp.end(), '(') == std::count(exp.begin(), exp.end(), ')'))
+    {
+        for (auto i = 0; i < exp.size(); i++)
+        {
+            //Identify numbers
+            if (isdigit(exp[i]))
             {
-                if ((isdigit(exp[i]) || exp[i] == '.'))
+                // eg. (2+3)2 --> is treated as (2+3)*2
+                if (prevTok == eTYPE_CLOSE_BRACE)
                 {
-                    test.push_back(exp[i]);
-                    i++;
+                    symbols.push('*');
                 }
-                else
+                // eg. (23.56 45.6) Is treated as a syntax error
+                else if (prevTok == eTYPE_DIGIT)
                 {
-                    break;
-                }    
-            } 
-            numbers.push(stof(test));
-            i--; 
+                    std::cout << "Syntax Error : Missing operator" << std::endl;
+                    return;
+                }
+
+                std::string digit_str;
+
+                /*After first occurance of a digit, iterate through the string 
+                to get the complete floating point number by checking the subsequent characters.
+                The end of a floating point number is identified by the next occurance of a symbol.*/
+                while (i < exp.size())
+                {
+                    if ((isdigit(exp[i]) || exp[i] == '.'))
+                    {
+                        digit_str.push_back(exp[i]);
+                        i++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                std::string::size_type sz;
+                digits.push(stof(digit_str, &sz));
+
+                // eg. (45 + 67.4556.34) is treated as a syntax error.
+                if (sz != digit_str.size())
+                {
+                    std::cout << "Syntax Error : Invalid floating point number" << std::endl;
+                    return;
+                }
+
+                i--;
+                prevTok = eTYPE_DIGIT;
+            }
+            //Identify symbols
+            else if (exp[i] == '(')
+            {
+                //2(2+3) --> is treated as 2*(2+3)
+                if (prevTok == eTYPE_DIGIT)
+                {
+                    symbols.push('*');
+                }
+                symbols.push(exp[i]);
+                prevTok = eTYPE_OPEN_BRACE;
+            }
+            else if (exp[i] == ')')
+            {
+                // eg. (2+ )+5 is treated as a syntax error
+                if (prevTok == eTYPE_OPERATOR)
+                {
+                    std::cout << "Syntax Error : Missing operand/Extra operator identified" << std::endl;
+                    return;
+                }
+                //If ')' is encountered evaluate the expression after the immediate previous '('
+                while (!symbols.empty() && symbols.top() != '(')
+                {
+                    apply(digits, symbols);
+                }
+                //Remove the previous corresponding '('
+                if (!symbols.empty())
+                {
+                    symbols.pop();
+                }
+                prevTok = eTYPE_CLOSE_BRACE;
+            }
+            //Ignore empty spaces
+            else if (std::isspace(exp[i]))
+            {
+                continue;
+            }
+            else if (validateOperator(exp[i]))
+            {
+                // eg. (2+ *3) is treated as a syntax error
+                if ((prevTok != eTYPE_DIGIT) && (prevTok != eTYPE_CLOSE_BRACE))
+                {
+                    std::cout << "Syntax Error : Missing operand/Extra operator identified" << std::endl;
+                    return;
+                }
+
+                /*If the symbol is an operator, check for precedence of previous operator in symbols stack
+                and evaluate the expression in the current brace.*/
+                while (!symbols.empty() && precedence(symbols.top()) >= precedence(exp[i]))
+                {
+                    apply(digits, symbols);
+                }
+                symbols.push(exp[i]);
+                prevTok = eTYPE_OPERATOR;
+            }
+            else
+            {
+                std::cout << "Syntax Error : Invalid character" << std::endl;
+                return;
+            }
         }
-        //Identify symbols
-        else if(exp[i] == '('){ 
-			symbols.push(exp[i]); 
-		}
-        //If ')' is encountered evaluate the expression after the immediate previous '('
-        else if(exp[i] == ')'){
-            while(!symbols.empty() && symbols.top() != '(') 
-			{ 
-                char op = symbols.top();
-				auto num1 = numbers.top();
-				numbers.pop(); 				
-				auto num2 = numbers.top(); 
-                numbers.pop();
-                auto res = operate(num2, num1, op); 
-				numbers.push(res); 
-                symbols.pop();
-			} 	 
-			if(!symbols.empty()){ 
-			symbols.pop();
-            } 
-        } 
-        //If the symbol is an operator add to the stack.
-        else if(exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/'){
-            symbols.push(exp[i]);
+        //Evaluate the final expression.
+        while (!symbols.empty())
+        {
+            apply(digits, symbols);
+        }
+
+        if (!digits.empty())
+        {
+            std::cout << digits.top() << std::endl;
+            digits.pop();
+        }
+    }
+    else
+    {
+        std::cout << "Syntax error : Mismatching Braces" << std::endl;
+    }
+}
+int main(int argc, char *argv[])
+{
+
+    std::ifstream ipfile(argv[1], std::ifstream::in);
+    std::string line;
+    std::vector<std::string> input;
+
+    //Read lines from the input file into a vector that stores only the expressions
+    if (ipfile.is_open())
+    {
+        if (ipfile.peek() == std::ifstream::traits_type::eof())
+        {
+            std::cout << "Input file is empty" << std::endl;
         }
         else
         {
-            std::cout<<"Invalid character : "<<exp[i]<<std::endl;
-            while(!symbols.empty()){
-                symbols.pop();
+            while (std::getline(ipfile, line))
+            {
+                //Ignore comments
+                if (line[0] != '#')
+                {
+                    input.push_back(line);
+                }
             }
-            while(!numbers.empty()){
-                numbers.pop();
-            }			
-            continue;
+            for (const auto &exp : input)
+            {
+                evaluate(exp);
+            }
         }
+        ipfile.close();
     }
-
-        //Validate the final operation
-        while(!symbols.empty()) 
-			{ 
-                char op = symbols.top();
-				auto num1 = numbers.top();
-				numbers.pop(); 				
-				auto num2 = numbers.top(); 
-                numbers.pop();
-                auto res = operate(num2, num1, op); 
-				numbers.push(res); 
-                symbols.pop();
-			}
-    
-            if(!numbers.empty()) 
-			{
-            std::cout<<"Output : "<<numbers.top()<<std::endl;
-            numbers.pop();
-            }
-}
-int main(int argc, char* argv[]) {
-
-  std::ifstream ipfile (argv[1]);
-  std::string line;
-  std::vector<std::string> input;
-
-  //Read lines from the input file into a vector that stores only the expressions
-  if (ipfile.is_open())
-  {
-    while(std::getline(ipfile,line))
-    {
-        //Ignore comments
-        if(line[0]!='#')
-        {
-            input.push_back(line);
-        }    
-    }
-    ipfile.close();
-  }
-  else 
-  std::cout << "Unable to open file"; 
-
-    for(auto exp:input)
-    {
-        evaluate(exp);
-    }
+    else
+        std::cout << "Unable to open file" << std::endl;
     return 0;
 }
